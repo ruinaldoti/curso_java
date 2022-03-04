a94a8819e4d29c7b32b42f3a5411d8a185fc4112
Ruinaldo <ruinaldo.ti@gmail.com>
2022-03-04 14:49:45 -0300

    Variavel Local

---
+++ Curso_Java/src/variaveis/Variavel_Local.java
@@ -0,0 +1,13 @@
+package variaveis;
+
+public class Variavel_Local {
+	/*Variavel Global*/
+	static int maiorIdadeGlobal = 2;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
