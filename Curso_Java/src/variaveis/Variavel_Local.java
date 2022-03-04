86c9b6db05af8a7694bbbe62fffc5e083f0598f0
Ruinaldo <ruinaldo.ti@gmail.com>
2022-03-04 15:21:51 -0300

    MediaEscolar

--- Curso_Java/src/variaveis/Variavel_Local.java
+++ Curso_Java/src/variaveis/Variavel_Local.java
@@ -1,12 +1,30 @@
 package variaveis;
 
 public class Variavel_Local {
-	/*Variavel Global*/
+	/* Variavel Global */
 	static int maiorIdadeGlobal = 2;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		int nota1 = 90;
+		int nota2 = 60;
+		int nota3 = 70;
+		int nota4 = 80;
+		int media= 0;
 		
+		media= (nota1 + nota2 + nota3 + nota4)/4;
+		
+		if(media >=70) {
+			System.out.println("Aluno Aprovado: "+ media);
+		}else {
+			if (media >= 40 && media <=69) {
+				System.out.println("Aluno em Recuperação: "+ media);
+			}
+			else {
+				System.out.println("Aluno Reprovado: " + media);
+			}
+		}
 
 	}
 
